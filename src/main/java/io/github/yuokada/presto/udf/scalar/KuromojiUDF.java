9df1dac1f1dab9f2569819b0d832e69ed700e2a3
yuokada <callistoiv+git@gmail.com>
2018-04-12 00:26:54 +0900

    fixup! Update UDF performance

--- src/main/java/io/github/yuokada/presto/udf/scalar/KuromojiUDF.java
+++ src/main/java/io/github/yuokada/presto/udf/scalar/KuromojiUDF.java
@@ -1,6 +1,8 @@
 package io.github.yuokada.presto.udf.scalar;
 
+import com.atilika.kuromoji.TokenizerBase;
 import com.atilika.kuromoji.ipadic.Tokenizer;
+import com.facebook.presto.spi.PrestoException;
 import com.facebook.presto.spi.block.Block;
 import com.facebook.presto.spi.block.BlockBuilder;
 import com.facebook.presto.spi.block.BlockBuilderStatus;
@@ -9,8 +11,14 @@ import com.facebook.presto.spi.function.LiteralParameters;
 import com.facebook.presto.spi.function.ScalarFunction;
 import com.facebook.presto.spi.function.SqlNullable;
 import com.facebook.presto.spi.function.SqlType;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import io.airlift.slice.Slice;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static com.facebook.presto.spi.StandardErrorCode.INVALID_FUNCTION_ARGUMENT;
 import static com.facebook.presto.spi.type.VarcharType.VARCHAR;
 import static io.airlift.slice.Slices.utf8Slice;
 
@@ -18,6 +26,17 @@ public class KuromojiUDF
 {
     private static final Tokenizer tokenizer = new Tokenizer();
     private static final Block zeroBlock = VARCHAR.createBlockBuilder(new BlockBuilderStatus(), 0).build();
+
+    private static Cache<String, Tokenizer> modeCache = CacheBuilder.newBuilder()
+            .maximumSize(3)
+            .build();
+
+    // https://github.com/google/guava/wiki/CachesExplained
+    private static final Cache<String, Tokenizer> cache = CacheBuilder.newBuilder()
+            .maximumSize(256)
+            .build();
+    // https://hivemall.incubator.apache.org/userguide/misc/tokenizer.html#japanese-tokenizer
+    private static final List<String> modes = Arrays.asList("normal", "search", "extended");
 
     @Description("Now Working.")
     @ScalarFunction("kuromoji_tokenize")
@@ -38,5 +57,53 @@ public class KuromojiUDF
 //            System.out.println(token.getPartOfSpeechLevel1().toString()); // => 名詞, 形容詞 or etc
                 });
         return blockBuilder.build();
+    }
+
+    @Description("Now Working.")
+    @ScalarFunction("kuromoji_tokenize")
+    @LiteralParameters("x")
+    @SqlType("array<varchar(x)>")
+    public static Block kuromojii(@SqlNullable @SqlType("varchar(x)") Slice sentence, @SqlType("varchar(x)") Slice mode)
+    {
+        String modeString = mode.toStringUtf8();
+        if (!modes.contains(modeString)) {
+            throw new PrestoException(INVALID_FUNCTION_ARGUMENT, "Invalid mode value: " + modeString);
+        }
+
+        if (sentence == null || sentence.toStringUtf8().isEmpty()) {
+            return zeroBlock;
+        }
+
+        String input = sentence.toStringUtf8();
+        BlockBuilder blockBuilder = VARCHAR.createBlockBuilder(new BlockBuilderStatus(), input.length() / 4);
+
+        Tokenizer tokenizer = getTokenizerWithMode(modeString);
+        tokenizer.tokenize(input).forEach(
+                token -> {
+                    VARCHAR.writeSlice(blockBuilder, utf8Slice(token.getSurface()));
+                });
+        return blockBuilder.build();
+    }
+
+    private static Tokenizer getTokenizerWithMode(String mode)
+    {
+        Tokenizer tokenizer = modeCache.getIfPresent(mode);
+        if (tokenizer == null) {
+            TokenizerBase.Mode modeObject;
+            switch (mode) {
+                case "search":
+                    modeObject = Tokenizer.Mode.SEARCH;
+                    break;
+                case "extended":
+                    modeObject = Tokenizer.Mode.EXTENDED;
+                    break;
+                case "normal":
+                default:
+                    modeObject = Tokenizer.Mode.NORMAL;
+            }
+            tokenizer = new Tokenizer.Builder().mode(modeObject).build();
+            modeCache.put(mode, tokenizer);
+        }
+        return tokenizer;
     }
 }
--- src/test/java/io/github/yuokada/presto/udf/scalar/KuromojiUDFTest.java
+++ src/test/java/io/github/yuokada/presto/udf/scalar/KuromojiUDFTest.java
@@ -5,11 +5,36 @@ import com.facebook.presto.spi.type.ArrayType;
 import com.facebook.presto.spi.type.VarcharType;
 import com.google.common.collect.ImmutableList;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class KuromojiUDFTest
         extends AbstractTestFunctions
 {
+    @DataProvider(name = "query-provider")
+    public Object[][] dataProvider()
+    {
+        return new Object[][] {
+                {"kuromoji_tokenize('5000兆円欲しい。')", 10,
+                 ImmutableList.of("5000", "兆", "円", "欲しい", "。")},
+                };
+    }
+
+    @DataProvider(name = "query-provider2")
+    public Object[][] dataProvider2()
+    {
+        return new Object[][] {
+                {"kuromoji_tokenize('5000兆円欲しい。', 'normal')", 10,
+                 ImmutableList.of("5000", "兆", "円", "欲しい", "。")},
+                {"kuromoji_tokenize('5000兆円欲しい。', 'search')", 10,
+                 ImmutableList.of("5000", "兆", "円", "欲しい", "。")},
+                {"kuromoji_tokenize('5000兆円欲しい。', 'extended')", 10,
+                 ImmutableList.of("兆", "円", "欲しい", "。", "5", "0", "0", "0")},
+                };
+    }
+
     @BeforeClass
     public void setUp()
     {
@@ -25,6 +50,20 @@ public class KuromojiUDFTest
                 ImmutableList.of("5000", "兆", "円", "欲しい", "。"));
     }
 
+    @Test(dataProvider = "query-provider")
+    public void testKuromojiNormalDP(String query, Integer size, List<String> expect)
+            throws Exception
+    {
+        assertFunction(query, new ArrayType(VarcharType.createVarcharType(size)), expect);
+    }
+
+    @Test(dataProvider = "query-provider2")
+    public void testKuromojiNormalWithMode(String query, Integer size, List<String> expect)
+            throws Exception
+    {
+        assertFunction(query, new ArrayType(VarcharType.createVarcharType(size)), expect);
+    }
+
     @Test
     public void testKuromojiEmpty()
             throws Exception
@@ -32,5 +71,12 @@ public class KuromojiUDFTest
         assertFunction("kuromoji_tokenize('')",
                 new ArrayType(VarcharType.createVarcharType(0)),
                 ImmutableList.of());
+    }
+
+    @Test
+    public void testInvalidArgument()
+    {
+        assertInvalidFunction("kuromoji_tokenize('', 'failValue')",
+                "Invalid mode value: failValue");
     }
 }
